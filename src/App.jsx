38c42eda681992f22defa72527e94f768ecfa51e
Victor Inojosa <vijoin@gmail.com>
2022-01-31 20:53:46 -0300

    Connect to metamask wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
 import screen_mask from './img/hSGyubQ.gif';
@@ -10,19 +10,64 @@ export default function App() {
     
   }
 
-  const checkIfWalletConnected = () => {
-    const { ethereum } = window;
+  const [currentAccount, setCurrentAccount] = useState("");
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-    } else {
-      console.log("We have the ethereum object", ethereum)
-    }
-  };
+  const checkIfWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+      } else {
+        console.log("We have the ethereum object", ethereum)
+      };
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+
+    } catch (error) {
+      console.log(error)
+      }
+  }
+
+    /**
+    * Implement your connectWallet method here
+    */
+
+    
+    const connectWallet = async () => {
+      try {
+        const { ethereum } = window;
+
+        if (!ethereum) {
+          alert("Get Metamask!");
+          return;
+        }
+
+        const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+
+        console.log("Connected", accounts[0]);
+        setCurrentAccount(accounts[0]);
+
+      } catch (error) {
+        console.log(error)
+      }
+    };
+
 
   useEffect(() => {
     checkIfWalletConnected();
-  }, [])
+  }, []);
   
   return (
     <div className="mainContainer">
@@ -41,6 +86,16 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+
       </div>
     </div>
   );
